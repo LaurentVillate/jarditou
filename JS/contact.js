4cfb9d5b208e3929f78320451b44678ea16e71e4
LaurentVillate <laurent.villate@yahoo.com>
2020-11-27 12:27:53 +0100

    exercice formulaire en cours

--- JS/contact.js
+++ JS/contact.js
@@ -1,15 +1,23 @@
-var nomvalid = document.getElementById("nom");
-    
 
-    function message()
-    {
-        alert("Le champ est invalide");
-        return;
-    }
-    
-    if (nomvalid<2)
-    {
-        nomvalid.onblur = message;
-    }
 
+document.getElementById("formulaire").addEventListener("submit", function(e) 
+{
+    e.preventDefault();
+    alert("envoyé");
     
+    
+});
+
+    var names = document.getElementByName("nom");
+    names.addEventListener("blur", verifnom );
+    function verifnom()
+    {
+        if (names.valueMissing)
+        names.onBlur = alert("attention)");
+    }
+   
+    var date = document.getElementById("datenaissance");
+    var postcode = document.getElementById("postal");
+    var adress = document.getElementById("adresse");
+    var city = document.getElementById("ville");
+    var email = document.getElementById("mail");
